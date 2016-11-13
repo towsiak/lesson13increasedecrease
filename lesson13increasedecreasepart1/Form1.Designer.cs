cb07944d8bd412599aab9270c81de3b39bd98fef
Tom Owsiak <towsiak@yahoo.com>
2016-11-13 01:24:49 -0500

    lessons 13/14 files

---
+++ lesson13increasedecreasepart1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace lesson13increasedecreasepart1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(398, 114);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show,Increase";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button2.Location = new System.Drawing.Point(544, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(398, 114);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button3.Location = new System.Drawing.Point(1094, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(398, 114);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Increase,Show";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.richTextBox1.Location = new System.Drawing.Point(13, 133);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(397, 504);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.richTextBox2.Location = new System.Drawing.Point(1094, 133);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(397, 504);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label1.Location = new System.Drawing.Point(544, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 63);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label2.Location = new System.Drawing.Point(134, 640);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 63);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "x++";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label3.Location = new System.Drawing.Point(1244, 650);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 63);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "++x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1535, 734);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lesson13increasedecreasepart1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lesson13increasedecreasepart1
+{
+    public partial class Form1 : Form
+    {
+        //int means values like -10, 0, 50, not 5.678
+        int x = 1;//this is present here so we have a variable we can use throughout our code
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = $"x={x}";//this is here so that the initial value of x can be shown on the form
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //+= has the effect of building up the output
+            //x++ has the action of first allowing us to display the value of x, then the value increased by 1
+            //\n at end stack output vertically by putting output on new lines
+            richTextBox1.Text += $"x={x++}\n";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = 1;//resets variable back to 1
+            richTextBox1.Text = "";//this clear the text from the box on the left side
+            richTextBox2.Text = "";//this clears the text from the box on the right side
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //+= has the effect of building up the output
+            //++x has the action of first updating the value of x, and then showing it
+            //\n at end stack output vertically by putting output on new lines
+            richTextBox2.Text += $"x={++x}\n";
+        }
+    }
+}
